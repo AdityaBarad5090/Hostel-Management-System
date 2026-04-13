8854f5bc12a536f91d51f4b8b7097ad9f22bf96f
AdityaBarad5090 <adityasinhbarad@gmail.com>
2026-04-13 16:39:26 +0530

    Delete Hostel Management System directory

--- Hostel Management System/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-
-router.post("/admin-login", authController.login);
-router.post("/student-login", authController.studentLogin);
-
-module.exports = router;
